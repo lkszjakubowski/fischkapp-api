e1f43e3a3a5f33a560a4d4427a484e3ce7ff3001
lvk <lksz.jakubowski@gmail.com>
2023-03-13 09:35:48 +0100

    added express server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello from Node.js!')
+const app = require('./app.js');
+const config = require('./utils/config.js');
+const logger = require('./utils/logger.js');
+
+app.listen(config.PORT, () => {
+  logger.info(
+    `Server running on port ${config.PORT} in ${config.NODE_ENV} mode.`
+  );
+});
