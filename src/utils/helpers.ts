3e9e86b568d87efa3a99b8ed02f3e1b5748bfc89
lvk <lksz.jakubowski@gmail.com>
2023-03-16 12:45:21 +0100

    edited deleting

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -9,16 +9,3 @@ export const lessThanFiveMinutes = (card: TCard): boolean => {
   }
   return false;
 };
-
-// if (card.createdAt) {
-//   const date = new Date();
-//   const fiveMinutes = 50 * 60 * 1000;
-//   const diffTime = date.getTime() - card.createdAt.getTime();
-//   if (diffTime < fiveMinutes) {
-//     await Card.findByIdAndDelete(req.params.id);
-//     res.status(204);
-//   } else {
-//     res.status(403).end();
-//   }
-// }
-// }
