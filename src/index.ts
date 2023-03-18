fefbcce616d408babc9708e3b81c1687a1630e97
lvk <lksz.jakubowski@gmail.com>
2023-03-18 10:11:41 +0100

    build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import config from './utils/config';
 import app from './app';
 
-app.listen(config.PORT, () => {
+app.listen(config.PORT || 4000, () => {
   console.log(
     `Server running on port ${config.PORT} in ${config.NODE_ENV} mode.`
   );
